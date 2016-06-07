8aedca2df4a5031393322baa492a1719d028ee0f
Ciyong Chen <ciyong.chen@intel.com>
2016-06-08 01:41:05 +0800

    update norm and add norm test case

---
+++ theano/tensor/nnet/tests/test_norm.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import, print_function, division
+from theano.tests import unittest_tools as utt
+import theano
+import numpy
+
+from theano.tensor.nnet.norm import 
+
+
+def test_NormAcrossMap():
+    pass
